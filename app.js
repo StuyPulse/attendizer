00e2af0bef4e82959145adf28462fe7a63519f1d
achen318 <50970130+achen318@users.noreply.github.com>
2022-05-10 08:21:46 -0400

    Separate server from the HTML

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
 
 // ========== ROUTES ==========
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
 
 // ========== SERVER ==========
 const port = process.env.PORT || 3000;
