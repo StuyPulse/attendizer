47241f342eba5e38745ee5722b49da36481e2284
Shroomicus <82099152+Shroomicus@users.noreply.github.com>
2022-05-18 16:06:14 -0400

    Reformat database.js

--- server/src/models/database.js
+++ server/src/models/database.js
@@ -2,23 +2,9 @@ const req = require('express/lib/request');
 const mysql = require('mysql2');
 const Sequelize = require('sequelize');
 const databaseConfig = require('../config/database_config.js');
-
-// const meeting = require('./meetings.js')(sequelizeInstance, Sequelize);
-// const student = require('./student.js')(sequelizeInstance, Sequelize);
-// const entry = require('./attendanceEntry')(sequelizeInstance, Sequelize);
-
-// const sequelizeInstance = new Sequelize(databaseConfig.DB, databaseConfig.USER, databaseConfig.PASSWORD, {
-//   host: databaseConfig.HOST,
-//   dialect: databaseConfig.dialect,
-//   operatorsAliases: false,
-
-//   pool: {
-//     max: databaseConfig.pool.max,
-//     min: databaseConfig.pool.min,
-//     acquire: databaseConfig.pool.acquire,
-//     idle: databaseConfig.pool.idle
-//   }
-// });
+const students = require('./student.js')(sequelizeInstance, Sequelize);
+const meetings = require('./meeting.js')(sequelizeInstance, Sequelize);
+const entries = require('./attendanceEntry.js')(sequelizeInstance, Sequelize);
 
 const sequelizeInstance = new Sequelize('sqlite::memory:');
 
@@ -33,10 +19,6 @@ const db = {};
 
 db.Sequelize = Sequelize;
 db.sequelizeInstance = sequelizeInstance;
-
-const students = require('./student.js')(sequelizeInstance, Sequelize);
-const meetings = require('./meeting.js')(sequelizeInstance, Sequelize);
-const entries = require('./attendanceEntry.js')(sequelizeInstance, Sequelize);
 
 (async () => {
   db.students = await students;
