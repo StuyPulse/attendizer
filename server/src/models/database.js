40ea419faf85b21f3e14b4e611c35abffcdea424
Shroomicus <82099152+Shroomicus@users.noreply.github.com>
2022-05-18 22:22:54 -0400

    Create initialize function for the database

--- server/src/models/database.js
+++ server/src/models/database.js
@@ -2,14 +2,14 @@ const req = require('express/lib/request');
 const mysql = require('mysql2');
 const Sequelize = require('sequelize');
 const databaseConfig = require('../config/database_config.js');
-const sequelizeInstance = new Sequelize('sqlite::memory:');
+const sequelize = new Sequelize('sqlite://attendance.sqlite3');
 
-const students = require('./student.js')(sequelizeInstance, Sequelize);
-const meetings = require('./meeting.js')(sequelizeInstance, Sequelize);
-const entries = require('./attendanceEntry.js')(sequelizeInstance, Sequelize);
+const students = require('./student.js')(sequelize, Sequelize);
+const meetings = require('./meeting.js')(sequelize, Sequelize);
+const entries = require('./attendanceEntry.js')(sequelize, Sequelize);
 
 try {
-  sequelizeInstance.authenticate();
+  sequelize.authenticate();
   console.log('Connection has been established successfully.');
 } catch (error) {
   console.error('Unable to connect to the database:', error);
@@ -18,15 +18,19 @@ try {
 const db = {};
 
 db.Sequelize = Sequelize;
-db.sequelizeInstance = sequelizeInstance;
+db.sequelize = sequelize;
 
-(async () => {
+async function initialize() {
   db.students = await students;
   db.meetings = await meetings;
   db.entries = await entries;
 
   db.students.belongsToMany(db.meetings, { through: db.entries });
   db.meetings.belongsToMany(db.students, { through: db.entries });
-})();
 
-module.exports = db;
+  await db.sequelize.sync({force : false});
+
+  return db;
+};
+
+module.exports = initialize;
