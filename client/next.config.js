dd06df62c65d3dd43f1df4b5feb5a9afd612d414
Anthony Chen <50970130+achen318@users.noreply.github.com>
2022-07-09 14:28:05 -0400

    Add server endpoints to config file

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    REG_URL: 'http://localhost:3000/api/reg',
+    SCAN_URL: 'http://localhost:3000/api/scan',
+    GET_URL: 'http://localhost:3000/api/sList'
+  }
+};
--- client/pages/admin.js
+++ client/pages/admin.js
@@ -1,19 +1,19 @@
 import Button from 'react-bootstrap/Button';
-import Meta from '../../components/Meta';
-import StudentEntry from '../../components/StudentEntry';
+import Meta from '../components/Meta';
+import StudentEntry from '../components/StudentEntry';
 import Table from 'react-bootstrap/Table';
-import styles from '../../styles/Home.module.css';
+import styles from '../styles/Home.module.css';
 import { useRouter } from 'next/router';
 
 export async function getServerSideProps() {
   // Fetches all students from backend
-  const res = await fetch('http://localhost:3000/api/sList');
+  const res = await fetch(process.env.GET_URL);
   const students = (await res.json()).students;
 
   return { props: { students } };
 }
 
-export default function Home({ students }) {
+export default function Admin({ students }) {
   const router = useRouter();
   const refreshData = () => {
     router.replace(router.asPath);
